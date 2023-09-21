c24526117c9e880451ff83200c3e1fa2f60086eb
durga-2003 <131599952+durga-2003@users.noreply.github.com>
2023-09-21 20:38:56 +0530

    Update dashboard06.py

--- dashboard06.py
+++ dashboard06.py
@@ -7,7 +7,7 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
-st.sidebar.header('Dashboard `version 2`')
+st.sidebar.header('Dashboard `version 1`')
 
 st.sidebar.subheader('Heat map parameter')
 time_hist_color = st.sidebar.selectbox('Color by', ('temp_min', 'temp_max')) 
@@ -18,11 +18,6 @@ donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
 st.sidebar.subheader('Line chart parameters')
 plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
 plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
-
-st.sidebar.markdown('''
----
-Created with ❤️ by [Data Professor](https://youtube.com/dataprofessor/).
-''')
 
 
 # Row A
